de585c828a8a68904de8e394035696053491e116
Aaron Chambers <achambers@gmail.com>
2015-02-13 15:02:33 +0000

    Starting on new version

---
+++ app/components/r-image.js
@@ -0,0 +1,17 @@
+import Em from 'ember';
+
+export default Em.Component.extend({
+  classNames: ['rotatable-image-container'],
+  attributeBindings: ['style'],
+  containerHeight: 0,
+
+  getInitialValues: function() {
+    var height = this.get('element').offsetHeight;
+    this.set('containerHeight', height);
+  }.on('didInsertElement'),
+
+  style: Em.computed('containerHeight', function() {
+    var height = this.get('containerHeight');
+    return 'line-height: ' + height + 'px;';
+  })
+});
