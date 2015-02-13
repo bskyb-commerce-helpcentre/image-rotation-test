7801fe8a8a0759d1a492ee635925320410a1fe93
Aaron Chambers and Peji Faghihi <achambers@gmail.com>
2015-02-13 17:06:23 +0000

    used universal scale function to rotate image

--- app/components/r-image.js
+++ app/components/r-image.js
@@ -13,5 +13,77 @@ export default Em.Component.extend({
   style: Em.computed('containerHeight', function() {
     var height = this.get('containerHeight');
     return 'line-height: ' + height + 'px;';
-  })
+  }),
+
+  imageStyle: Em.computed('degrees', 'scale', function() {
+    var degrees = this.get('degrees');
+    var scale   = this.get('scale');
+    return 'transform: rotate('+ degrees +'deg) scale('+ scale +');';
+  }),
+
+  aspect: Em.computed('aspect', function() {
+    var height = this.$().height();
+    var width = this.$().width();
+    return width/height;
+  }),
+
+  imageWidth: function() {
+    return this.$('img').width();
+  }.property(),
+
+  imageHeight: function() {
+    return this.$('img').height();
+  }.property(),
+
+  imageAspect: Em.computed('imageAspect', 'imageHeight', 'imageWidth', function() {
+    var height = this.get('imageHeight');
+    var width = this.get('imageWidth');
+    return width/height;
+  }),
+
+  degrees: Em.computed(function() {
+    return 0;
+  }),
+
+  scale: Em.computed(function() {
+    return 1;
+  }),
+
+  click: function() {
+    this.incrementProperty('degrees', 90);
+    this.swapImageWidthAndHeight();
+    this.set('scale', this.recalculateScale());
+  },
+
+  //mouseEnter: function() {
+    //this.incrementProperty('degrees', 3);
+  //},
+
+  //mouseLeave: function() {
+    //this.decrementProperty('degrees', 3);
+  //},
+
+  recalculateScale: function() {
+    var containerAspect = this.get('aspect');
+    var imageAspect = this.get('imageAspect');
+
+    var imageHeight = this.get('imageHeight');
+    var imageWidth = this.get('imageWidth');
+    var containerHeight = this.$().height();
+    var containerWidth = this.$().width();
+
+    if (imageAspect > containerAspect) {
+      return containerWidth / imageWidth;
+    } else {
+      return containerHeight / imageHeight;
+    }
+  },
+
+  swapImageWidthAndHeight: function() {
+    var height = this.get('imageHeight');
+    var width = this.get('imageWidth');
+    this.set('imageHeight', width);
+    this.set('imageWidth', height);
+  }
+
 });
